77b8e4b1d27f887dfa2fb5c1fc5878e10d7c389e
dimalexa <dimalexa.07@gmail.com>
2025-01-22 14:27:12 +0300

    many many changes

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def download_schedule(message: Message):
 
 @dp.callback_query(F.data == 'button_schedule')  # [2]
 async def get_schedule(callback: CallbackQuery):
-    document = FSInputFile('schedule.pdf')
+    document = FSInputFile('data/schedule.pdf')
     await bot.send_document(callback.from_user.id, document)
     await callback.answer()
 
